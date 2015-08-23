c52af116c4ed44dc5c32af8175d2ad8a9aa36897
SekthDroid <secthndamon@gmail.com>
2015-08-23 23:30:37 +0200

    Added the second test case, where the program should return the number if only one is passed

--- strcalculator/string_calculator.py
+++ strcalculator/string_calculator.py
@@ -3,4 +3,6 @@ __author__ = 'SekthDroid'
 
 class StringCalculator(object):
     def add(self, param):
+        if param:
+            return int(param)
         return 0
--- test/test_stringCalculator.py
+++ test/test_stringCalculator.py
@@ -6,6 +6,14 @@ __author__ = 'SekthDroid'
 
 class TestStringCalculator(TestCase):
 
+
+    def setUp(self):
+        super().setUp()
+        self.calculator = StringCalculator()
+
     def test_should_return_0_when_empty_string_is_passed(self):
-        string_calculator = StringCalculator()
-        self.assertEqual(0, string_calculator.add(""))
+        self.assertEqual(0, self.calculator.add(""))
+
+    def test_should_return_number_if_only_1_number_is_passed(self):
+        self.assertEqual(1, self.calculator.add("1"))
+        self.assertEqual(2, self.calculator.add("2"))
